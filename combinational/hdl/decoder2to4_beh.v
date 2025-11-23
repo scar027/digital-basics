1c735d00e0b3d769ab3fa1b0ae1c37d01ae8bba4
scar027 <shantanupai@hotmail.com>
2025-11-24 00:02:57 +0530

    Fix indentation issue
    Signed-off-by: scar027 <shantanupai@hotmail.com>

--- combinational/hdl/decoder2to4_beh.v
+++ combinational/hdl/decoder2to4_beh.v
@@ -1,16 +1,16 @@
 module decoder2to4_beh
 (
-        input [1:0] in,
-        output reg [3:0] out
+	input [1:0] in,
+	output reg [3:0] out
 );
-        always @(*) begin
-                out = 4'b0000;
-                case(in)
-                        2'b00: out[0] = 1;
-                        2'b01: out[1] = 1;
-                        2'b10: out[2] = 1;
-                        2'b11: out[3] = 1;
+	always @(*) begin
+		out = 4'b0000;
+		case(in)
+			2'b00: out[0] = 1;
+			2'b01: out[1] = 1;
+			2'b10: out[2] = 1;
+			2'b11: out[3] = 1;
 			default: out = 4'bxxxx;
-                endcase
-        end
+		endcase
+	end
 endmodule
